ce2bcaebf33456fd577cd521202fb8446b971528
Elizabeth Clawson <emclawson1@gmail.com>
2024-09-13 14:12:20 -0500

    added posts model and post get and post routes

--- index.js
+++ index.js
@@ -4,35 +4,66 @@ import connectDb from "./db.js"
 
 
 import Grade from "./models/grades.js"
+import Post from "./models/posts.js"
 
 const app = express();
 const port = 3000;
 
 app.use(express.json())
 
+//Grade Routes 
 app.get('/', async (req, res) => {
-    const results = await Grade.find({ class_id: 300 }).limit(5)
-    res.send(results)
+    const results = await Grade.find({ class_id: 311 }).limit(5)
     console.log(results)
+    res.send(results)
 })
 
 //Grade Routes 
+
+//working in reqbin and in my nodemon server but not showing up in compass 
 app.post('/', async (req, res) => {
-    const gradeDoc = new Grade({
-        scores: [],
-        class_id: req.body.class_id,
-        learner_id: req.body.learner_id,
-    })
-    const result =  await gradeDoc.save();
+    try { 
+        console.log(req.body)
+        console.log(typeof req.body.class_id)
+        console.log(typeof req.body.learner_id)
+        const result = await Grade.create({
+            scores: [],
+            class_id: req.body.class_id, 
+            learner_id: req.body.learner_id
+        })
     console.log(result)
     res.send(result)
+    } catch(e) {
+        console.log(e)
+    }
 })
 
-// app.get('/:class_id', async (req, res) => {
-//     const result = await Grade.find({ class_id: 300 }).limit(5)
-//     res.send(result)
-//     console.log(result)
-// })
+// post Routes 
+
+//giving me a status of 200 but not displaying anything on the page 
+app.get('/posts', async (req, res) => {
+    const results = await Post.find({}).limit(5)
+    console.log(results)
+    res.send(results)
+})
+
+app.post('/posts', async (req, res) => {
+    try { 
+        console.log(req.body)
+        console.log(typeof req.body.body)
+        console.log(typeof req.body.author)
+        console.log(typeof req.body.title)
+        const result = await Post.create({
+            body: req.body.body,
+            author: req.body.author, 
+            title: req.body.title
+        })
+    console.log(result)
+    res.send(result)
+    } catch(e) {
+        console.log(e)
+    }
+})
 
 app.listen(port, () => {
     console.log(`server is listening on port: ${port}`);
--- models/posts.js
+++ models/posts.js
@@ -1,2 +1,18 @@
 import mongoose from "mongoose";
 
+const postSchema = new mongoose.Schema({ //this is the new model we made for grades
+    body: {
+        type: String, 
+        required: true,
+    },
+    author:{ 
+        type: String, 
+        required: true,
+    },
+    title:{ 
+        type: String, 
+        required: true,
+    },
+})
+
+export default mongoose.model('post', postSchema) 
