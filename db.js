749c01a1557574a02f78f30d8b29fedf08f866ac
Elizabeth Clawson <emclawson1@gmail.com>
2024-09-13 13:16:24 -0500

    general push

--- db.js
+++ db.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
-
 const connectDb = async () => {
-    try{ 
+    try {
         await mongoose.connect(process.env.ATLAS_URI)
-        console.log("mongodb connected") //so you know it works 
-    } catch (error) {
-        console.log(error)
+        console.log('MongoDB connected...')
+    } catch(e) {
+        console.log(e)
     }
 }
-
 export default connectDb;
 
--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 import express from 'express';
 // import mongoose from 'mongoose';
 // import 'dotenv/config'
-import db from "./db.js"
+import connectDb from "./db.js"
 import { ObjectId } from 'mongodb';
 
-// import Grade from "./models/grades.js"
+import Grade from "./models/grades.js"
+import { connect } from 'mongoose';
 const app = express();
 const port = 3000;
 
 app.use(express.json())
 
+// await mongoose.connect(process.env.ATLAS_URI);
+
 app.get('/', async (req,res) => {
-    try {
-        let collection = await db.collection("posts")
-        let results = await collection.find({}).limit(50).toArray()
-        res.json(results).status(200)
-    } catch(e) {
-        res.json(e).status(400)
-    }
+    let result = await Grade.find({ class_id: 311 });
+    res.send(result)
 })
 
 app.use((err, _req, res, next) => {
@@ -26,4 +24,5 @@ app.use((err, _req, res, next) => {
 
 app.listen(port, () => {
     console.log(`server is listening on port: ${port}`);
+    connectDb()
 });
