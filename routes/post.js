0eb0da5e8af01b4e87b8204153ace9d05f21f717
Elizabeth Clawson <emclawson1@gmail.com>
2024-09-16 23:17:38 -0500

    updated patch route for posts

--- routes/post.js
+++ routes/post.js
@@ -30,6 +30,17 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.patch("/:id", async(req,res) => {
+    try{ 
+        const query = { _id: new ObjectId(req.params.id)};
+        const result = await Post.updateOne(query, { $set: {body: req.body.body },});
+        res.send(result).status(200)
+    } catch (e) {
+        console.log(e)
+        res.send(e).status(404)
+    }
+  })
+
 router.delete("/:id", async(req,res) =>{
     try {
        const query = { _id: new ObjectId(req.params.id)}
